e5b32983fa82525e33b894cc987b0391ec4cb9cd
Rafael Velloso <46040272+Errexis@users.noreply.github.com>
2021-10-20 21:44:39 -0300

    Update

--- index.py
+++ index.py
@@ -1,47 +1,67 @@
-import getpass
-import hashlib
+from tkinter import *
+from functools import partial
+import time
 import os
+
 clear = lambda: os.system('cls')
+janela = Tk()
+janela.title("MENU")
+
+def login():
+    print("1 2 3 Login")
+    janela.title("Login")
+
+    lb2 = Label(janela, text="Login:")
+    lb2.grid(row=0,column=0)
+    passwordEntry = Entry(janela)
+    passwordEntry.grid(row=0,column=1)
+
+    lb3 = Label(janela, text="Senha:")
+    lb3.grid(row=1,column=0)
+    ep = StringVar
+    ed2 = Entry(janela, textvariable=ep, show="*")
+    ed2.grid(row=1,column=1)
+
+    bt1 = Button(janela, text="Confirmar", command=Checkbutton)
+    bt1.grid(row=2,column=1)
+    janela.geometry("300x300")
+    janela.mainloop()
+
+def register():
+    print("1 2 3 Register")
+    janela.title("Registre-se")
+
+    lb2 = Label(janela, text="Login:")
+    lb2.grid(row=0,column=0)
+    passwordEntry = Entry(janela)
+    passwordEntry.grid(row=0,column=1)
+
+    lb3 = Label(janela, text="Senha:")
+    lb3.grid(row=1,column=0)
+    ep = StringVar
+    ed2 = Entry(janela, textvariable=ep, show="*")
+    ed2.grid(row=1,column=1)
+
+    lb4 = Label(janela, text="Confirmar Senha:")
+    lb4.grid(row=2,column=0)
+    ep = StringVar
+    ed3 = Entry(janela, textvariable=ep, show="*")
+    ed3.grid(row=2,column=1)
+
+    bt1 = Button(janela, text="Confirmar", command=Checkbutton)
+    bt1.grid(row=3,column=1)
+    janela.geometry("300x300")
+    janela.mainloop()
 
 def main():
-    clear()
-    print("    MENU    ")
-    print("            ")
-    print("1 - Login")
-    print("2 - Registrar")
-
-    while True:
-        print()
-        opcaous = input("Escolha uma opção: ")
-        if opcaous in ['1','2']:
-            break
-    if opcaous == 1:
-        login()
-    else: 
-        registrar()
-
-def registrar():
-    clear() 
-    print("Registre-se:")
-    while True:
-        print()
-        nomeus = input("Nome: ").title()
-        if nomeus != '':
-            break
-        nomeus = sanitizeName(nomeus)
-        if userAlreadyExist(nomeus):
-            displayUserAlreadyExistMessage()
-        else: 
-            while True:
-                senhaus = getpass("Senha: ")
-                if senhaus != '':
-                    break
-            while True:
-                csenhaus = getpass("Senha: ")
-                if senhaus != '':
-                    break
-            else:
-                print("Senha Incorreta! ")
-                print()
+    lb1 = Label(janela, text="Escolha uma opção abaixo")
+    lb1.grid(row=0,column=1)
+    bt2 = Button(janela, text="Login", command=login)
+    bt2.grid(row=1,column=1)
+    bt3 = Button(janela, text="Registrar", command=register)
+    bt3.grid(row=2,column=1)
+    janela.geometry("300x300")
+    janela.mainloop()
+    
 
 main()
