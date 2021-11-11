86cd8e392e537854de033f304e5c373b41705ad7
Rafael Velloso <46040272+Errexis@users.noreply.github.com>
2021-11-10 21:46:26 -0300

    Interface aplicada

--- main.py
+++ main.py
@@ -15,56 +15,32 @@ c = conn.cursor()
 colorbg = "#4935F1"
 bt = "#9187E1"
 camp = "#B5B3C1"
+colorerro = "#ff0000"
 #Janela Tkinter
 root = Tk()
 root.geometry("200x200")
 root.configure(bg=colorbg)
 
-def main(): #Menu para login/Registro
-    root.title("Login")
-    root.configure(bg=colorbg)
-
-    lb = Label(root, text="Stark - Logística")
-    lb.place(x=10 ,y=10)
-    lb.configure(bg=colorbg)
-    lb2 = Label(root, text="Usuário:")
-    lb2.place(x=10 ,y=35)
-    lb2.configure(bg=colorbg)
-    login = Entry(root, border=0)
-    login.place(x=10 ,y=60)
-    login.configure(bg=camp)
-
-    lb3 = Label(root, text="Senha:")
-    lb3.place(x=10 ,y=90)
-    lb3.configure(bg=colorbg)
-    ep = StringVar
-    ed2 = Entry(root, textvariable=ep, show="*", border=0)
-    ed2.place(x=10 ,y=110)
-    ed2.configure(bg=camp)
-    #criar conta
-    cr = Button(root, text="Criar conta", command=registrar, bg=colorbg, border=0, cursor="hand2", activebackground=colorbg)
-    cr.place(x=10 ,y=130)
-    
-    bt1 = Button(root, text="Logar", command=logar, border=0, cursor="hand2", activebackground=colorbg)
-    bt1.place(x=10 ,y=170)
-    bt1.configure(bg=bt )
-    root.geometry("210x210")
-    root.mainloop()
-
 def logar(): #Função de Logar 
     try:
-        c.execute("SELECT senha FROM contas WHERE user ='{}'".format(lb2))
-        contas = c.fetchall()
-        for x in range(1000):  
-            if (lb3 == contas[i]):
-                clear()
-                print("Autenticado com sucesso!")
-                escolha()
-            else: 
-                print("Senha incorreta.")
-            conn.close()
+        user = login.get()
+        password = senha.get()
+        c.execute("SELECT * FROM contas WHERE user='"+user+"' and senha='"+password+"'")
+        contas = c.fetchone()  
+        if contas:
+            clear()
+            print("Autenticado com sucesso!")
+            escolha()
+        else: 
+            eruser = Label(root, text="Usuário/senha inválido.")
+            eruser.place(x=10 ,y=160)
+            eruser.configure(bg=colorbg,fg=colorerro , border=0)
+        conn.close()
     except:
-        print("Usuário inválido")
+        er = Label(root, text="Algo deu Errado.")
+        er.place(x=10 ,y=160)
+        er.configure(bg=colorbg,fg=colorerro , border=0)
+
     
 def registrar(): #Menu de Registro
     root.destroy()
@@ -121,23 +97,31 @@ def registro(): #Função de registrar
 
 def escolha(): #Menu de escolha
     root.destroy()
-    janela4 = Tk()
-    janela4.title("Menu de Escolha")
-    janela4.geometry("180x200")
+    janela = Tk()
+    janela.title("Menu de Escolha")
+    janela.geometry("180x200")
     janela.configure(bg=colorbg)
-    lb = Label(janela4, text="Selecione uma opção: " )
-    lb.place(x=10,y=15)
-    bt = Button(janela4, text="Operario" )
-    bt.place(x= 10, y=35)
-    bt2 = Button(janela4, text="Gerente" )
-    bt2.place(x= 10, y=55)
-    bt3 = Button(janela4, text="Compras" )
-    bt3.place(x= 10, y=75)
-    bt4 = Button(janela4, text="Logistica" )
-    bt4.place(x= 10, y=95)
-    bt5 = Button(janela4, text="Sair" )
-    bt5.place(x= 10, y=115)
-    if bt == 1:
+
+    lb = Label(janela, text="Selecione uma opção: " )
+    lb.place(x=20,y=15)
+    lb.configure(bg=colorbg, border=0)
+
+    bt = Button(janela, text="Operario", border=0, cursor="hand2", activebackground=colorbg)
+    bt.place(x= 20, y=40)
+
+    bt2 = Button(janela, text="Gerente", border=0, cursor="hand2", activebackground=colorbg)
+    bt2.place(x= 20, y=65)
+
+    bt3 = Button(janela, text="Compras", border=0, cursor="hand2", activebackground=colorbg)
+    bt3.place(x= 20, y=90)
+
+    bt4 = Button(janela, text="Logistica", border=0, cursor="hand2", activebackground=colorbg)
+    bt4.place(x= 20, y=115)
+    
+    bt5 = Button(janela, text="Sair", border=0, cursor="hand2", activebackground=colorbg)
+    bt5.place(x= 20, y=155)
+    
+"""     if bt == 1:
         Operario.main()
     elif bt2 == 2:
         Gerente.main()
@@ -148,6 +132,36 @@ def escolha(): #Menu de escolha
     elif bt5 == 9:
         return
     else:
-        escolha()
+        er = Label(janela, text="Escolha uma opção correta!")
+        er.place(x=20,y=135) """
     
-main()
+
+root.title("Login")
+root.configure(bg=colorbg)
+
+lb = Label(root, text="Stark - Logística")
+lb.place(x=10 ,y=10)
+lb.configure(bg=colorbg)
+lb2 = Label(root, text="Usuário:")
+lb2.place(x=10 ,y=35)
+lb2.configure(bg=colorbg)
+login = Entry(root, border=0)
+login.place(x=10 ,y=60)
+login.configure(bg=camp)
+
+lb3 = Label(root, text="Senha:")
+lb3.place(x=10 ,y=90)
+lb3.configure(bg=colorbg)
+ep = StringVar
+senha = Entry(root, textvariable=ep, show="*", border=0)
+senha.place(x=10 ,y=110)
+senha.configure(bg=camp)
+#criar conta
+cr = Button(root, text="Criar conta", command=registrar, bg=colorbg, border=0, cursor="hand2", activebackground=colorbg)
+cr.place(x=10 ,y=130)
+
+bt1 = Button(root, text="Logar", command=logar, border=0, cursor="hand2", activebackground=colorbg)
+bt1.place(x=10 ,y=185)
+bt1.configure(bg=bt )
+root.geometry("210x210")
+root.mainloop()
